5a95e31775049d74731f1e049c13c0ca79818487
EYuan <ericyuany@sjtu.edu.cn>
2025-05-16 13:37:41 +0800

    feat: german hallo on the third hello world

--- ex1.c
+++ ex1.c
@@ -3,6 +3,6 @@ int main()
 {
     printf("Hello world!!\n");
     printf("Hello world again!\n");
-    printf("Hello world again and again!\n");
+    printf("Hallo world again and again!\n");
     return 0;
- }
+}
