fe9cc0c53bf654ea67a20803c0dada6eb75ad32a
EYuan <ericyuany@sjtu.edu.cn>
2025-05-16 14:22:28 +0800

    feat: ex4 min function completed

--- ex4.c
+++ ex4.c
@@ -1,3 +1,7 @@
 //write two functions that finds the maximum and minimum of three numbers
 #include <stdio.h>
 #include <stdlib.h>
+int min(int a, int b, int c)
+{
+    return (a < b) ? ((a < c) ? a : c) : ((b < c) ? b : c);
+}
