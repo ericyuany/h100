cb9c1073762b4a3cb62e0125f3306c21fe6cb0d7
EYuan <ericyuany@sjtu.edu.cn>
2025-05-16 14:19:36 +0800

    feat: max funciton completed

--- ex4.c
+++ ex4.c
@@ -1,3 +1,7 @@
 //write two functions that finds the maximum and minimum of three numbers
 #include <stdio.h>
 #include <stdlib.h>
+int max(int a, int b, int c)
+{
+    return (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+}
