3f99d7739b7e962406ac1b4f995ed2b7f0ae99b3
EYuan <ericyuany@sjtu.edu.cn>
2025-05-16 14:03:55 +0800

    feat: main funciton of ex4 completed

---
+++ ex4.c
@@ -0,0 +1,13 @@
+//write two functions that finds the maximum and minimum of three numbers
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a, b, c;
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+    printf("The maximum of %d, %d, and %d is: %d", a, b, c, max(a, b, c));
+    printf("The minimum of %d, %d, and %d is: %d", a, b, c, min(a, b, c));
+    return 0;
+}
